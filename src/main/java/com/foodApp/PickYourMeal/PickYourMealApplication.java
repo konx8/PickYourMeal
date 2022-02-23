f1cd22cdd31d7c63b36eefc28ff615a71a7c5772
Konrad <konx90@gmail.com>
2022-02-23 19:22:54 +0100

    Project init

---
+++ src/main/java/com/foodApp/PickYourMeal/PickYourMealApplication.java
@@ -0,0 +1,13 @@
+package com.foodApp.PickYourMeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickYourMealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickYourMealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodApp/PickYourMeal/PickYourMealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodApp.PickYourMeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickYourMealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
